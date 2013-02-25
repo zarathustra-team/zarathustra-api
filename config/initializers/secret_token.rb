14fdfb72411708fc1e5fd0d8b35b62e3c368356d
Lookis <lucas@diandian.com>
2013-02-25 17:42:35 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ZarathustraApi::Application.config.secret_token = 'ea242c90f20f044efffe4d1591168412b56b4cc3aa71769d314e8ebef68c37865ab49e9bd087b572c37b54ed1efb1f9b60946723e770659487c1b9463d838bb6'
